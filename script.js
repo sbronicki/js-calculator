9cbe7e46b52bf5a44949ec9d0111d760e5f550be
sbronicki <sbronicki@gmail.com>
2021-01-11 12:24:08 -0500

    evaluation displayed

--- script.js
+++ script.js
@@ -1,41 +1,97 @@
 class Calculator {
-	constructor(outputScreenText, formulaScreenText) {
-		this.formulaScreenText = formulaScreenText;
-		this.outputScreenText = outputScreenText;
+	constructor(outputScreenTextElement, formulaScreenTextElement) {
+		this.formulaScreenTextElement = formulaScreenTextElement;
+		this.outputScreenTextElement = outputScreenTextElement;
 		this.zeroScreen();
 	}
-	formulaScreen = '';
+	formulaScreenText = '';
+	outputScreenText = '';
 	outputScreen = '';
+	inputCount = 0;
+	inputLimit = 12;
+	inputArray = [];
+	inputResult;
+
 	zeroScreen() {
-		this.formulaScreenText.innerText = '0';
-		this.outputScreenText.innerText = '0';
+		this.formulaScreenTextElement.innerText = '0';
+		this.outputScreenTextElement.innerText = '0';
 		this.op = undefined;
+		this.formulaScreenText = '';
+		this.outputScreenText = '';
+		this.inputCount = 0;
+		this.inputArray = [];
+	}
+	zeroOutputScreen() {
+		this.outputScreenTextElement.innerText = '0';
+		this.outputScreenText = '';
 	}
 	appendEntry(entry) {
+		if (this.inputCount === this.inputLimit) return;
+
 		if (entry === '.' && this.outputScreen.includes('.')) return;
-		this.formulaScreen += entry.toString();
-		this.outputScreen += entry.toString();
+		this.formulaScreenText += entry.toString();
+		this.outputScreenText += entry.toString();
+		this.inputCount++;
 	}
-	chooseOp(op) {}
-	evaluate() {}
+	chooseOp(op) {
+		// need if to see if last entry was op if so switch op
+		if (this.formulaScreenText.indexOf(op) !== -1) console.log('yes');
+
+		this.op = op;
+		this.inputArray.push(parseFloat(this.outputScreenText));
+		this.inputArray.push(this.op);
+	}
+	evaluate() {
+		this.inputArray.push(parseFloat(this.outputScreenText));
+		this.inputResult = this.inputArray.join('');
+		let finalResult = eval(this.inputResult);
+		this.inputArray = [];
+		this.inputResult = [];
+		console.log(finalResult);
+		this.outputScreenText = finalResult;
+	}
 	updateDisplay() {
-		this.formulaScreenText.innerText = this.formulaScreen;
-		this.outputScreenText.innerText = this.outputScreen;
+		this.formulaScreenTextElement.innerText = this.formulaScreenText;
+		this.outputScreenTextElement.innerText = this.outputScreenText;
 	}
 }
-const formulaScreenText = document.getElementById('formula-screen');
-const outputScreenText = document.getElementById('output-screen');
+const formulaScreenTextElement = document.getElementById('formula-screen');
+const outputScreenTextElement = document.getElementById('output-screen');
 const numbers = document.querySelectorAll('[data-number]');
 const numsAndOps = document.querySelectorAll('[data-on-screen]');
 const op = document.querySelectorAll('[data-op]');
 const equals = document.getElementById('equals');
 const clear = document.getElementById('clear');
 
-const calculator = new Calculator(outputScreenText, formulaScreenText);
+const calculator = new Calculator(outputScreenTextElement, formulaScreenTextElement);
 
-numsAndOps.forEach((button) => {
+numbers.forEach((button) => {
 	button.addEventListener('click', () => {
 		calculator.appendEntry(button.innerText);
 		calculator.updateDisplay();
 	});
 });
+op.forEach((button) => {
+	button.addEventListener('click', () => {
+		calculator.chooseOp(button.innerText);
+		calculator.appendEntry(button.innerText);
+		calculator.updateDisplay();
+		calculator.zeroOutputScreen();
+	});
+});
+clear.addEventListener('click', () => {
+	calculator.zeroScreen();
+});
+
+equals.addEventListener('click', (button) => {
+	calculator.evaluate();
+	calculator.updateDisplay();
+});
+
+// still need to
+// set char limit error message
+// handle output when char limit reach (#e^n notation)
+// keep zero infront of decimal if op was last entry
+// changing op
+// nagative number inputs
+// template row minmax handle infitite expression
