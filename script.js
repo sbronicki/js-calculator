e48c82498aa16e799acf942e5f25bcb0da900eb9
sbronicki <sbronicki@gmail.com>
2021-01-11 22:14:55 -0500

    final stlye rework

--- script.js
+++ script.js
@@ -1,3 +1,129 @@
+// class Calculator {
+// 	constructor(outputScreenTextElement, formulaScreenTextElement) {
+// 		this.formulaScreenTextElement = formulaScreenTextElement;
+// 		this.outputScreenTextElement = outputScreenTextElement;
+// 		this.zeroScreen();
+// 	}
+// 	formulaScreenText = '';
+// 	outputScreenText = '';
+// 	outputScreen = '';
+// 	inputCount = 0;
+// 	inputLimit = 100;
+// 	outputLimit = 13;
+// 	outputCount = 0;
+// 	inputArray = [];
+// 	inputResult;
+
+// 	zeroScreen() {
+// 		this.formulaScreenTextElement.innerText = '0';
+// 		this.outputScreenTextElement.innerText = '0';
+// 		this.op = undefined;
+// 		this.formulaScreenText = '';
+// 		this.outputScreenText = '';
+// 		this.inputCount = 0;
+// 		this.inputArray = [];
+// 	}
+// 	zeroOutputScreen() {
+// 		this.outputScreenTextElement.innerText = '0';
+// 		this.outputScreenText = '';
+// 	}
+// 	appendEntry(entry) {
+// 		if (this.inputCount === this.inputLimit) {
+// 			this.outputScreenText = 'Char limit';
+// 			// setTimeout(() => {
+// 			// 	this.outputScreenText = 'Char limit';
+// 			// }, 2000);
+// 			return;
+// 		}
+// 		if (entry === '.' && this.outputScreen.includes('.')) return;
+// 		// negative nums
+// 		// if (
+// 		// 	this.inputArray[this.inputCount] === '-' &&
+// 		// 	this.formulaScreenText.indexOf('-') === this.formulaScreenText.length()
+// 		// ) {
+// 		// 	console.log('double negative');
+// 		// 	this.inputArray.pop();
+// 		// 	// this.inputArray.push(`-${this.formulaScreenText}`);
+// 		// 	console.log(this.inputArray);
+// 		// }
+
+// 		// this.outputCount = entry.toString().length();
+// 		// console.log(this.outputCount);
+// 		this.formulaScreenText += entry.toString();
+// 		this.outputScreenText += entry.toString();
+// 		this.inputCount++;
+// 	}
+// 	chooseOp(op) {
+// 		// need if to see if last entry was op if so switch op
+// 		// if (this.formulaScreenText.indexOf(op) !== -1) console.log('yes');
+// 		// if (this.inputArray[this.inputCount - 1] === '+' || '-' || '*' || '/') {
+// 		// 	console.log(this.inputArray);
+// 		// }
+// 		this.op = op;
+// 		this.inputArray.push(parseFloat(this.outputScreenText));
+// 		this.inputArray.push(this.op);
+// 	}
+// 	evaluate() {
+// 		this.inputArray.push(parseFloat(this.outputScreenText));
+// 		this.inputResult = this.inputArray.join('');
+// 		let finalResult = Math.round(eval(this.inputResult) * 10000) / 10000;
+// 		this.inputArray = [];
+// 		this.inputResult = [];
+// 		this.outputScreenText = finalResult;
+// 	}
+// 	updateDisplay() {
+// 		this.formulaScreenTextElement.innerText = this.formulaScreenText;
+// 		this.outputScreenTextElement.innerText = this.outputScreenText;
+// 	}
+// 	makeNegative(positiveVal) {
+// 		return positiveVal - 2 * positiveVal;
+// 	}
+// }
+// const formulaScreenTextElement = document.getElementById('formula-screen');
+// const outputScreenTextElement = document.getElementById('output-screen');
+// const numbers = document.querySelectorAll('[data-number]');
+// const numsAndOps = document.querySelectorAll('[data-on-screen]');
+// const op = document.querySelectorAll('[data-op]');
+// const equals = document.getElementById('equals');
+// const clear = document.getElementById('clear');
+
+// const calculator = new Calculator(outputScreenTextElement, formulaScreenTextElement);
+
+// numbers.forEach((button) => {
+// 	button.addEventListener('click', () => {
+// 		calculator.appendEntry(button.innerText);
+// 		calculator.updateDisplay();
+// 	});
+// });
+// op.forEach((button) => {
+// 	button.addEventListener('click', () => {
+// 		calculator.chooseOp(button.innerText);
+// 		calculator.appendEntry(button.innerText);
+// 		calculator.updateDisplay();
+// 		calculator.zeroOutputScreen();
+// 	});
+// });
+// clear.addEventListener('click', () => {
+// 	calculator.zeroScreen();
+// });
+
+// equals.addEventListener('click', (button) => {
+// 	calculator.evaluate();
+// 	calculator.updateDisplay();
+// });
+
+// // still need to
+// // changing op
+// // nagative number inputs
+
+// // set char limit error message
+
+// // keep zero infront of decimal if op was last entry
+
+// // template row minmax handle infitite expression
+
+// // new calc if entries after equals
+
 class Calculator {
 	constructor(outputScreenTextElement, formulaScreenTextElement) {
 		this.formulaScreenTextElement = formulaScreenTextElement;
@@ -8,9 +134,13 @@ class Calculator {
 	outputScreenText = '';
 	outputScreen = '';
 	inputCount = 0;
-	inputLimit = 12;
-	inputArray = [];
-	inputResult;
+	inputLimit = 14;
+	screenCount = 0;
+	screenLimit = 100;
+	screenArray = [];
+	screenResult;
+	evaluated = false;
+	allButtons = document.querySelectorAll('button');
 
 	zeroScreen() {
 		this.formulaScreenTextElement.innerText = '0';
@@ -18,37 +148,52 @@ class Calculator {
 		this.op = undefined;
 		this.formulaScreenText = '';
 		this.outputScreenText = '';
+		this.screenCount = 0;
 		this.inputCount = 0;
-		this.inputArray = [];
+		this.screenArray = [];
 	}
 	zeroOutputScreen() {
 		this.outputScreenTextElement.innerText = '0';
 		this.outputScreenText = '';
 	}
 	appendEntry(entry) {
-		if (this.inputCount === this.inputLimit) return;
-
+		if (this.inputCount === this.inputLimit) {
+			this.outputScreenText = 'Char limit';
+			console.log(this.screenArray, this.screenArray[this.inputCount]);
+			// setTimeout(() => {
+			// 	console.log(this.outputScreenText);
+			// 	// this.outputScreenText = 'Char limit';
+			// }, 2000);
+			return;
+		}
+		if (this.screenCount === this.screenLimit) return;
+		if (this.evaluated) {
+			this.zeroScreen();
+			this.evaluated = false;
+		}
 		if (entry === '.' && this.outputScreen.includes('.')) return;
 		this.formulaScreenText += entry.toString();
 		this.outputScreenText += entry.toString();
+		this.screenCount++;
 		this.inputCount++;
 	}
 	chooseOp(op) {
 		// need if to see if last entry was op if so switch op
-		if (this.formulaScreenText.indexOf(op) !== -1) console.log('yes');
+		// if (this.formulaScreenText.indexOf(op) !== -1) console.log('yes');
 
 		this.op = op;
-		this.inputArray.push(parseFloat(this.outputScreenText));
-		this.inputArray.push(this.op);
+		this.screenArray.push(parseFloat(this.outputScreenText));
+		this.screenArray.push(this.op);
+		this.inputCount = 0;
 	}
 	evaluate() {
-		this.inputArray.push(parseFloat(this.outputScreenText));
-		this.inputResult = this.inputArray.join('');
-		let finalResult = eval(this.inputResult);
-		this.inputArray = [];
-		this.inputResult = [];
-		console.log(finalResult);
+		this.screenArray.push(parseFloat(this.outputScreenText));
+		this.screenResult = this.screenArray.join('');
+		let finalResult = Math.round(eval(this.screenResult) * 10000) / 10000;
+		this.screenArray = [];
+		this.screenResult = [];
 		this.outputScreenText = finalResult;
+		this.evaluated = true;
 	}
 	updateDisplay() {
 		this.formulaScreenTextElement.innerText = this.formulaScreenText;
@@ -83,15 +228,15 @@ clear.addEventListener('click', () => {
 	calculator.zeroScreen();
 });
 
-equals.addEventListener('click', (button) => {
+equals.addEventListener('click', () => {
 	calculator.evaluate();
 	calculator.updateDisplay();
 });
 
 // still need to
-// set char limit error message
-// handle output when char limit reach (#e^n notation)
-// keep zero infront of decimal if op was last entry
-// changing op
-// nagative number inputs
 // template row minmax handle infitite expression
+// show char limit error message
+// >>>>>>>>>> diable buttons> remove massage after 2 seconds>all functionality
+// handle output when char limit reach (#e^n notation)
+// changing op
+// nagative number screens
