d9009469e138dec492b2156c1cc432d8776a6ccb
sbronicki <sbronicki@gmail.com>
2021-01-12 19:27:44 -0500

    new calc with last eval possible

--- script.js
+++ script.js
@@ -125,116 +125,134 @@
 // // new calc if entries after equals
 
 class Calculator {
-	constructor(outputScreenTextElement, formulaScreenTextElement) {
+	constructor(inputScreenTextElement, formulaScreenTextElement) {
 		this.formulaScreenTextElement = formulaScreenTextElement;
-		this.outputScreenTextElement = outputScreenTextElement;
+		this.inputScreenTextElement = inputScreenTextElement;
 		this.zeroScreen();
 	}
 	formulaScreenText = '';
-	outputScreenText = '';
-	outputScreenCurrent = '';
+	inputScreenText = '';
+	inputScreenCurrent = '';
+	// will be lower when char limit error is handled properly (not required)
 	inputCount = 0;
-	inputLimit = 14;
+	inputLimit = 500;
 	screenCount = 0;
-	screenLimit = 100;
+	screenLimit = 500;
+	opRan = false;
+	///////////////////////////////////////////////////////////////////////////
 	screenArray = [];
+	entryArray = [];
 	screenResult;
 	evaluated = false;
+	finalResult;
+	opVal;
 
 	zeroScreen() {
 		this.formulaScreenTextElement.innerText = '0';
-		this.outputScreenTextElement.innerText = '0';
+		this.inputScreenTextElement.innerText = '0';
 		this.op = undefined;
 		this.formulaScreenText = '';
-		this.outputScreenText = '';
+		this.inputScreenText = '';
 		this.screenCount = 0;
 		this.inputCount = 0;
 		this.screenArray = [];
+		this.finalResult = undefined;
 	}
-	zeroOutputScreen() {
-		this.outputScreenTextElement.innerText = '0';
-		this.outputScreenText = '';
+	zeroInputScreen() {
+		this.inputScreenTextElement.innerText = '0';
+		this.inputScreenText = '';
 	}
 	appendEntry(entry) {
-		// need to refactor but it works for now
-		if (this.inputCount === this.inputLimit) {
-			if (this.outputScreenCurrent === this.outputScreenTextElement.innerText) {
-				console.log('other if');
-				this.outputScreenText = 'Char limit';
-				for (let btn of allButtons) {
-					btn.disabled = true;
-					setTimeout(() => {
-						this.outputScreenTextElement.innerText = this.outputScreenCurrent;
-					}, 1499);
-					setTimeout(() => {
-						for (let btn of allButtons) {
-							btn.disabled = false;
-						}
-					}, 2000);
-					return;
-				}
-			}
-			this.outputScreenCurrent = this.outputScreenText;
-			this.screenArray.push(parseFloat(this.outputScreenCurrent));
-			this.outputScreenText = 'Char limit';
-			for (let btn of allButtons) {
-				btn.disabled = true;
-			}
-			setTimeout(() => {
-				this.outputScreenTextElement.innerText = this.outputScreenCurrent;
-			}, 1499);
-			setTimeout(() => {
-				for (let btn of allButtons) {
-					btn.disabled = false;
-				}
-			}, 2000);
+		// char limit error handling
+		// if (this.inputCount === this.inputLimit) {
+		// 	this.inputScreenCurrent = this.inputScreenText;
+		// 	this.screenArray.push(parseFloat(this.inputScreenCurrent));
+		// 	this.inputScreenText = 'Char limit';
+		// 	for (let btn of allButtons) {
+		// 		btn.disabled = true;
+		// 	}
+		// 	setTimeout(() => {
+		// 		this.inputCount = 0;
+		// 		this.inputScreenTextElement.innerText = '0';
+		// 	}, 1499);
+		// 	setTimeout(() => {
+		// 		for (let btn of allButtons) {
+		// 			btn.disabled = false;
+		// 		}
+		// 	}, 1500);
+		// 	return;
+		// }
+		// if (this.screenCount === this.screenLimit) return;
 
+		// u.s. - #14: runnin da op after eval begins new calc with previous eval result
+		if (this.finalResult && this.opRan === true) {
+			console.log(this.finalResult);
+			this.formulaScreenText = `${this.finalResult}${this.opVal}`;
+			this.evaluated = false;
+			this.finalResult = undefined;
 			return;
 		}
-		if (this.screenCount === this.screenLimit) return;
+
+		// u.s. - #10: cant start w/ multilple zeros
+		// console.log(entry);
+		// this.entryArray.push(entry);
+		// if (this.entryArray[0] === '0' && this.entryArray[1] === '0') return;
+
 		if (this.evaluated) {
 			this.zeroScreen();
 			this.evaluated = false;
 		}
-		if (entry === '.' && this.outputScreen.includes('.')) return;
+		if (entry === '.' && this.inputScreenText.includes('.')) return;
+
 		this.formulaScreenText += entry.toString();
-		this.outputScreenText += entry.toString();
+		this.inputScreenText += entry.toString();
 		this.screenCount++;
 		this.inputCount++;
 	}
 	chooseOp(op) {
-		// need if to see if last entry was op if so switch op
+		// u.s. - #13: 2 or more op input consecutively op run should be last entered (excluding the negative (-) sign)
 		// if (this.formulaScreenText.indexOf(op) !== -1) console.log('yes');
 
+		// char limit error handling
+		// this.opRan = true;
+		// if (
+		// 	parseFloat(this.inputScreenText) === false ||
+		// 	parseFloat(this.inputScreenText) !== parseFloat(this.inputScreenText)
+		// ) {
+		// 	this.inputCount = 0;
+		// 	return;
+		// }
+		this.opRan = true;
 		this.op = op;
-		this.screenArray.push(parseFloat(this.outputScreenText));
+		this.screenArray.push(parseFloat(this.inputScreenText));
 		this.screenArray.push(this.op);
 		this.inputCount = 0;
+		this.opVal = op;
 	}
 	evaluate() {
-		this.screenArray.push(parseFloat(this.outputScreenText));
+		this.screenArray.push(parseFloat(this.inputScreenText));
 		this.screenResult = this.screenArray.join('');
-		let finalResult = Math.round(eval(this.screenResult) * 10000) / 10000;
+		this.finalResult = Math.round(eval(this.screenResult) * 10000) / 10000;
 		this.screenArray = [];
 		this.screenResult = [];
-		this.outputScreenText = finalResult;
+		this.inputScreenText = this.finalResult;
 		this.evaluated = true;
+		this.opRan = false;
 	}
 	updateDisplay() {
 		this.formulaScreenTextElement.innerText = this.formulaScreenText;
-		this.outputScreenTextElement.innerText = this.outputScreenText;
+		this.inputScreenTextElement.innerText = this.inputScreenText;
 	}
 }
 const formulaScreenTextElement = document.getElementById('formula-screen');
-const outputScreenTextElement = document.getElementById('output-screen');
+const inputScreenTextElement = document.getElementById('output-screen');
 const numbers = document.querySelectorAll('[data-number]');
-const numsAndOps = [ ...document.querySelectorAll('[data-on-screen]') ];
+// const numsAndOps = [ ...document.querySelectorAll('[data-on-screen]') ];
 const op = document.querySelectorAll('[data-op]');
 const equals = document.getElementById('equals');
 const clear = document.getElementById('clear');
-const allButtons = [ ...document.querySelectorAll('button') ];
 
-const calculator = new Calculator(outputScreenTextElement, formulaScreenTextElement);
+const calculator = new Calculator(inputScreenTextElement, formulaScreenTextElement);
 
 numbers.forEach((button) => {
 	button.addEventListener('click', () => {
@@ -247,7 +265,7 @@ op.forEach((button) => {
 		calculator.chooseOp(button.innerText);
 		calculator.appendEntry(button.innerText);
 		calculator.updateDisplay();
-		calculator.zeroOutputScreen();
+		calculator.zeroInputScreen();
 	});
 });
 clear.addEventListener('click', () => {
@@ -260,8 +278,9 @@ equals.addEventListener('click', () => {
 });
 
 // still need to
-// show char limit error message
-// >>>>>>>>>> diable buttons> remove massage after 2 seconds>all functionality
-// handle output when char limit reach (#e^n notation)
-// changing op
-// nagative number screens
+// u.s. - #10: cant start w/ multilple zeros
+// u.s. - #13: 2 or more op input consecutively op run should be last entered (excluding the negative (-) sign)
+// u.s. - #14: runnin da op after eval begins new calc with previous eval result
+
+// not necessary: if '.' = first entry, put a zero in front of it
+// not necessary: imput limit of 14, show error message -> zero input & be able to continue calc
